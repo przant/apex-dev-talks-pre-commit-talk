fcb682b4669a859cef31db2d7e7a97f005c2d9a3
Antonio Pérez <apema2079@gmail.com>
2025-04-02 17:01:01 -0600

    feat: example completed

---
+++ test_python.py
@@ -0,0 +1,10 @@
+def hello():
+    return "Hello, World!"
+
+# This is a comment with some words spelled wrong on purpose
+def sum(a,b):
+    return a+b
+
+if __name__ == "__main__":
+    print(hello())
+    print(sum(5,5))
