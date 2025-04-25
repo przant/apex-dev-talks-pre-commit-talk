c8809b6a9151551dc0d6412b08e2ca5b5c33c52f
Antonio Pérez <apema2079@gmail.com>
2025-04-25 15:13:59 -0600

    feat: add sample project files with intenational issues

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# The script above should be executable but isn't - will trigger check-shebang-scripts-are-executable
+
+from setuptools import setup, find_packages
+
+# Misspelled word - will trigger codespell
+setup(
+    name="sample_project",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pyyaml",
+    ],
+    author="Sample Author",
+    author_email="author@example.com",
+    description="A sample project for demonstarting pre-commit hooks",
+    keywords="sample, pre-commit",
+    python_requires=">=3.6",
+)
